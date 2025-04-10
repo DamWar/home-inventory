c2d1fd0b5c845ceada35800b3aa5f6ae94493319
DamWar <d.warzecha@poczta.fm>
2025-04-10 19:25:50 +0200

    refactor with black
    With some exceptions. black source: https://github.com/psf/black

--- code/main.py
+++ code/main.py
@@ -1,10 +1,10 @@
-#. .venv/bin/activate
+# . .venv/bin/activate
 import csv
 from flask import Flask, render_template
 from pathlib import Path
 
 app = Flask(__name__)
-default_color = '#FFFFFF'
+default_color = "#FFFFFF"
 
 
 class Enclosure:
@@ -15,7 +15,7 @@ class Enclosure:
     def assing_image(self, image_path: Path):
         self.image_path = image_path
 
-    def assign_properties(self, position=[0,0], size=[0,0], color=default_color):
+    def assign_properties(self, position=[0, 0], size=[0, 0], color=default_color):
         self.position = position
         self.size = size
         self.color = color
@@ -24,41 +24,47 @@ class Enclosure:
         self.s_object.append(s_object)
 
 ### Those subclasses are entirely for easier readability of inventory() code ###
-class Room(Enclosure): 
+class Room(Enclosure):
     def assign_container_group(self, container_groups: object):
         super()._assign_smaller_object(container_groups)
-class ContainerGroup(Enclosure): 
+class ContainerGroup(Enclosure):
     def assign_container(self, containers: object):
         super()._assign_smaller_object(containers)
-class Container(Enclosure): 
+class Container(Enclosure):
     def assign_item(self, item: object):
         super()._assign_smaller_object(item)
-    
 
 class Item:
-    def __init__(self, name: str, quantity = 1, subitems = []):
+    def __init__(self, name: str, quantity=1, subitems=[]):
         self.name = name
         self.quantity = quantity
-        self.subitems = subitems # example: pencil(subitem) inside a small pencils_box(Item) within a big art_box(Container)
+        self.subitems = subitems  # example: pencil(subitem) inside a small pencils_box(Item) within a big art_box(Container)
 
 
 def read_container_csv(file: str) -> list[object]:
-    with open(file, newline='') as csv_file: #I decided to use csv first, since I don't know the final database structure and I'd rather just code right now instead of contemplating my containers' structure
+    with open(
+        file, newline=""
+    ) as csv_file:  # I decided to use csv first, since I don't know the final database structure and I'd rather just code right now instead of contemplating my containers' structure
         container = csv.DictReader(csv_file)
         line_count = 0
         items = []
         for item in container:
-            if item.get("quantity") is None: #default in dict.get() doesn't work if None is already set (it only works when index is not set)
+            if (
+                item.get("quantity") is None
+            ):  # default in dict.get() doesn't work if None is already set (it only works when index is not set)
                 item["quantity"] = 1
-            item_object = Item(item.get("name"), item.get("quantity"), item.get("subitems"))
+            item_object = Item(
+                item.get("name"), item.get("quantity"), item.get("subitems")
+            )
             items.append(item_object)
             if line_count == 0:
                 print(f'Column names are {", ".join(item)}')
                 line_count += 1
             print(f'\t{item["name"]} is stored in quantity of {item["quantity"]}.')
             line_count += 1
-        print(f'Processed {line_count} lines.')
+        print(f"Processed {line_count} lines.")
     return items
+
 
 def get_subpaths(path: Path) -> list[str]:
     subpaths = []
@@ -71,29 +77,30 @@ def get_subpaths(path: Path) -> list[str]:
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route('/inventory')
+
+@app.route("/inventory")
 def inventory():
     rooms_list = []
-    
+
     ## TODO: Refactor into more universal code, which utilizes Enclosure with each directory and Container with *.csv items
     ## TODO: Upon refactoring, add self.type to Enclosure (room, etc.)
     ## Note1: There may be issues with displaying on website, unless very detailed yaml file is provided for each directory, so I'm leaving it for later improvements since I only need current structure
     ## Note2: It might also not be possible due to jinja2 templates' limitation
-    rooms_path = Path('code/containers')
+    rooms_path = Path("code/containers")
     rooms = get_subpaths(rooms_path)
-    for room in rooms: # example: living_room/
+    for room in rooms:  # example: living_room/
         if room.is_dir():
             room_obj = Room(room.name)
 
             container_groups_path = room
             container_groups = get_subpaths(container_groups_path)
-            for container_group in container_groups: # example: cabinet
+            for container_group in container_groups:  # example: cabinet
                 if container_group.is_dir():
                     container_group_obj = ContainerGroup(container_group.name)
 
                     containers_path = container_group
                     containers = get_subpaths(containers_path)
-                    for container in containers: # example: shelf_1.csv
+                    for container in containers:  # example: shelf_1.csv
                         if container.is_file():
                             if container.name == "bg.png": # TODO: (very low priority - efficiency optimization) look for unique files only once, with path generated in above loops, instead of checking every container iteration
                                 container_group_obj.assing_image(containers_path.joinpath("bg.png"))
@@ -104,9 +111,11 @@ def inventory():
                                 container_group_obj.assign_container(container_obj)
 
                                 items = read_container_csv(container)
-                                for item in items: # example: "pen, 1"
+                                for item in items:  # example: "pen, 1"
                                     item_obj = Item(item.name)
                                     container_obj.assign_item(item_obj)
                     room_obj.assign_container_group(container_group_obj)
         rooms_list.append(room_obj)
-    return render_template('main.html', rooms=rooms_list) #rooms contain all other objects
+    return render_template(
+        "main.html", rooms=rooms_list
+    )  # rooms contain all other objects
